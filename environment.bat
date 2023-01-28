05abe3af786d2353826125c71094241a02efb7c0
imgai-newbey <122021096+imgai-newbey@users.noreply.github.com>
2023-01-28 16:43:15 +0900

    Update environment.bat

--- environment.bat
+++ environment.bat
@@ -2,9 +2,9 @@
 
 set DIR=%~dp0cache
 
-set PATH=C:\Windows\system32;C:\Windows;%DIR%\git\bin;%DIR%\python;%DIR%\python\Scripts
-set PY_LIBS=%DIR%\python\Scripts\Lib;%DIR%\python\Scripts\Lib\site-packages
-set PY_PIP=%DIR%\python\Scripts
+set PATH=C:\Windows\system32;C:\Windows;%DIR%\git\bin;%DIR%\Python;%DIR%\Python\Scripts
+set PY_LIBS=%DIR%\Python\Scripts\Lib;%DIR%\Python\Scripts\Lib\site-packages
+set PY_PIP=%DIR%\Python\Scripts
 set SKIP_VENV=1
-set PIP_INSTALLER_LOCATION=%DIR%\python\get-pip.py
+set PIP_INSTALLER_LOCATION=%DIR%\Python\get-pip.py
 set TRANSFORMERS_CACHE=%DIR%\transformers-cache
